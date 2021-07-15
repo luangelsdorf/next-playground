b63b4d8f1b8528bcf5f769266cbcf774ede22248
lulaluano <luanferreira2136@gmail.com>
2021-07-14 22:36:46 -0300

    vscode commit

--- pages/request-animation-frame/index.js
+++ pages/request-animation-frame/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import styles from '../../public/css/modules/request-animation-frame.module.css';
 
 export default function requestAnimationFrame() {
   return (
-    <div>a</div>
+    <article>
+      <p className={styles.bgRed}>Lorem ipsum,</p>
+      <div className={styles.w50}>
+        <p className={styles.bgGreen}>dolor,</p>
+      </div>
+      <section>
+        <span className={styles.bgGreen}>dolor,</span>
+      </section>
+      <p className={styles.bgBlue}>sit amet.</p>
+    </article>
   )
 }
